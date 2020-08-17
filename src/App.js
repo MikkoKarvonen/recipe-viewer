6a2dfa041e1a59c5eb6ca6a327b21c56db8b35fe
Mikko <mkarvonen34@gmail.com>
2020-08-17 17:25:43 +0300

    Simple display for data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get(`http://localhost:1337/foods`).then((res) => {
+      setData(res.data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.map((e) => {
+        return (
+          <div>
+            <h1>{e.Name}</h1>
+            <small>{e.Duration} min</small>
+            <h2>Ingredients</h2>
+            <ul>
+              {e.Ingredient.map((f) => {
+                return (
+                  <li>
+                    {f.Ingredient} - {f.Amount} {f.Unit}
+                  </li>
+                );
+              })}
+            </ul>
+            <h2>Steps</h2>
+            <ul>
+              {e.Steps.map((f, i) => {
+                return (
+                  <li>
+                    <h3>Step {i + 1}</h3>
+                    <small>{f.Duration} min</small>
+                    <h3>Ingredients for this step</h3>
+                    <ul>
+                      {f.Ingredient.map((g) => {
+                        return (
+                          <li>
+                            {g.Ingredient} - {g.Amount} {g.Unit}
+                          </li>
+                        );
+                      })}
+                    </ul>
+                    {f.Guide}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        );
+      })}
     </div>
   );
 }
